5b6d3c43ebc359134a973397c812d7a8fb582b40
Adotel <adriandotel@gmail.com>
2023-03-25 16:11:46 +0100

    Agregar archivos de proyecto.

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+
+#include "../header/todo.h"
+#include <iostream>
+
+using namespace std;
+
+void print_menu() {
+
+}
+
+int main() {
+	
+	int opt;
+	string name_list;
+
+	cout << "Introduce el nombre de la lista : ";
+	cin >> name_list;
+
+	TodoList list_app(name_list);
+
+	do {
+
+		cout << "Introduce una opcion: ";
+		cin >> opt;
+
+		if (opt == 1) {
+			cout << list_app.get_Name() << endl;
+		}
+		else if (opt == 2) {
+			Task test = { 50, "Nombre generico", HIGH, true };
+			list_app.add_Task(test);
+		}
+		else if (opt == 3) {
+			list_app.list_Tasks();
+		}
+
+	} while (opt != 0);
+
+	return 0;
+}
---
+++ src/todo.cpp
@@ -0,0 +1,77 @@
+
+#include "../header/todo.h"
+#include <iostream>
+
+// Helper
+
+void print_task(Task& _task) {
+	std::cout << "Tarea " << _task.id << std::endl;
+	std::cout << _task.get_Name() << std::endl;
+	std::cout << _task.get_Priority() << std::endl;
+	std::cout << _task.is_Finished() << std::endl;
+}
+
+// TodoList declaration
+
+TodoList::TodoList(std::string _name) { this->name = _name; };
+
+std::string TodoList::get_Name() { return this->name; };
+
+int TodoList::get_ListLength() { return this->list_length;  };
+
+void TodoList::add_Task(Task& _task){
+	this->list[0] = _task;
+	++this->list_length;
+}
+
+void TodoList::remove_Task(int _id) {
+
+	int position_task;
+
+	for (int i = 0; i < this->list_length; ++i){
+		if (list[i].id == _id) {
+			position_task = i;
+			break;
+		}
+	}
+
+	for (int i = position_task; i < this->list_length - 1; ++i) {
+		this->list[i] = this->list[i + 1];
+	}
+
+	--this->list_length;
+}
+
+void TodoList::list_Tasks() {
+
+	for (int i = 0; i < this->list_length; ++i) {
+		print_task(this->list[i]);
+	}
+}
+
+// Task
+
+std::string Task::get_Name() { return this->task_name; }
+
+Priority Task::get_Priority() { return this->priority; }
+
+bool Task::is_Finished() { return this->finished;  }
+
+std::string Task::set_Name(std::string _name) {
+	this->task_name = _name;
+	return this->task_name;
+}
+
+Priority Task::set_Priority(Priority _priority) {
+	this->priority = _priority;
+	return this->priority;
+};
+
+
+bool Task::set_ChangeFinished() {
+	this->finished = !this->finished;
+	return this->finished;
+}
+
+
+
